87432143af9965a1943d90c32778d7df04720e43
Robert4S <robertschmidt4th@gmail.com>
2025-04-19 15:10:59 +0200

    FIX: cleaned up files

--- src.fish
+++
@@ -1,8 +0,0 @@
-function test_prog
-	for i in (seq $argv[1]); $argv[2] > ./lines.txt; end
-	if test (uniq < ./lines.txt | wc -l) -eq 1
-    		echo "Results are consistent"
-	else
-    		echo "!!!!! Results are not consistent"
-	end
-end
